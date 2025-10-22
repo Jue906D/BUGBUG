72a35eeb12126890be98f2ebec8d3b75d3657be9
Louna0228 <332611027@qq.com>
2025-10-22 21:55:05 +0800

    [page]dev- search

---
+++ GitPage/js/newsData.js
@@ -0,0 +1,17 @@
+
+const newsList = [
+  { title: "世纪之交的危机——千年虫", url: "route01post00.html", date: "1999-12-09" },
+  { title: "张千年：有信心击退“千年虫”", url: "route01post01.html", date: "1999-12-08" },
+  { title: "张千年先生逝世：享年38岁", url: "route01post02.html", date: "1999-12-07" },
+  { title: "不明飞行物现象", url: "404.html", date: "1999-12-10" },
+  { title: "成龙'绯闻'事件", url: "route01post02.html", date: "1999-11-19" },
+  { title: "登陆火星", url: "404.html", date: "1999-12-14" },
+  { title: "第三届国际足联女子世界杯", url: "404.html", date: "1999-6-18" },
+  { title: "二进制的祖宗：从《易经》和阴阳调和说起", url: "404.html", date: "1997-5-6" },
+  { title: "张千年教授的电脑课堂-揭秘时间戳", url: "route01post03.html", date: "1999-07-19" },
+  { title: "“千年虫”卷土重来？揭秘2038年虫", url: "404.html", date: "2038-10-14" },
+  { title: "“万年虫”再度示警，专家称：祸害遗万年", url: "404.html", date: "11451-17-36" },
+  { title: "最后一周，千万奖金，等你来拿！", url: "404.html", date: "1999-12-18" },
+  { title: "火星上可能存在智慧生命", url: "404.html", date: "1999-12-18" },
+  { title: "震惊！竟然使用地沟油！", url: "404.html", date: "1999-12-18" }
+];
